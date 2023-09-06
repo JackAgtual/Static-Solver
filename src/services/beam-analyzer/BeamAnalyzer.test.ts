ed010f2ea13247aaabb243daa31ccafcf8c4d091
Jack Agtual <jaagtual@gmail.com>
2023-09-05 21:32:50 -0700

    Mocking supports and loads getter methods on Beam

--- src/services/beam-analyzer/BeamAnalyzer.test.ts
+++ src/services/beam-analyzer/BeamAnalyzer.test.ts
@@ -1,12 +1,18 @@
 import BeamAnalyzer from './BeamAnalyzer'
 import Beam from '../beam/Beam'
 
-// TODO: Mock Beam
 describe('BeamAnalyzer', () => {
   describe('solveReactionForces', () => {
     let beam: Beam
+    let beamAnalyzer: BeamAnalyzer
+    let getLoadsMock = jest.spyOn(Beam.prototype, 'loads', 'get')
+    let getSupportsMock = jest.spyOn(Beam.prototype, 'supports', 'get')
+
     beforeEach(() => {
       beam = new Beam(20)
+      beamAnalyzer = new BeamAnalyzer(beam)
+      getLoadsMock.mockClear()
+      getSupportsMock.mockClear()
     })
 
     it.todo('gets loads from beam')
@@ -16,28 +22,32 @@ describe('BeamAnalyzer', () => {
     it.todo('throws error if system is not static')
 
     it('throws error if statically indeterminate', () => {
-      beam.addSupport({ x: 10, rfy: true, rfx: false, rmz: false })
-      beam.addSupport({ x: 9, rfy: true, rfx: false, rmz: false })
-      beam.addSupport({ x: 8, rfy: true, rfx: false, rmz: false })
-      beam.addLoad({ x: 10, fx: 100, fy: -300, mz: 0 })
-      const beamAnalyzer = new BeamAnalyzer(beam)
+      getSupportsMock.mockReturnValue([
+        { id: 1, x: 10, rfy: true, rfx: false, rmz: false },
+        { id: 2, x: 9, rfy: true, rfx: false, rmz: false },
+        { id: 3, x: 8, rfy: true, rfx: false, rmz: false },
+      ])
+      getLoadsMock.mockReturnValue([{ id: 1, x: 10, fx: 100, fy: -300, mz: 0 }])
+
       expect(() => beamAnalyzer.solveReactionForces()).toThrowError()
 
-      beam = new Beam(20)
-      beam.addLoad({ x: 20, fy: -200, fx: 0, mz: 0 })
-      beam.addSupport({ x: 0, rfy: true, rfx: false, rmz: false })
+      getLoadsMock.mockClear().mockReturnValue([{ id: 1, x: 20, fy: -200, fx: 0, mz: 0 }])
+      getSupportsMock
+        .mockClear()
+        .mockReturnValue([{ id: 1, x: 0, rfy: true, rfx: false, rmz: false }])
       const beamAnalyzerWithTooFewSupports = new BeamAnalyzer(beam)
       expect(() => beamAnalyzerWithTooFewSupports.solveReactionForces()).toThrowError()
     })
 
-    it.only('correctly solves for reaction forces', () => {
-      beam.addSupport({ x: 0, rfx: true, rfy: true, rmz: true })
-      beam.addLoad({ x: 20, fx: 100, fy: -300, mz: 0 })
-      const beamAnalyzer = new BeamAnalyzer(beam)
+    it('correctly solves for reaction forces', () => {
+      getSupportsMock.mockReturnValue([{ id: 1, x: 0, rfx: true, rfy: true, rmz: true }])
+      getLoadsMock.mockReturnValue([{ id: 1, x: 20, fx: 100, fy: -300, mz: 0 }])
+
       const supports = beamAnalyzer.solveReactionForces()
+
       expect(supports[0]).toBe(-100)
       expect(supports[1]).toBe(300)
-      expect(supports[1]).toBe(6000)
+      expect(supports[2]).toBe(6000)
     })
   })
 })
