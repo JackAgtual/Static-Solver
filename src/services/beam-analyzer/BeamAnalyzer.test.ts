46d7350095e4f15a134d94d9be1f75d25947ae32
Jack Agtual <jaagtual@gmail.com>
2023-09-13 22:23:02 -0700

    Adding test to make sure unknowns can be solved in an order

--- src/services/beam-analyzer/BeamAnalyzer.test.ts
+++ src/services/beam-analyzer/BeamAnalyzer.test.ts
@@ -73,5 +73,69 @@ describe('BeamAnalyzer', () => {
       expect(solvedSupports[1]).toBe(300)
       expect(solvedSupports[2]).toBe(6000)
     })
+
+    it('returns values based on the order of supports', () => {
+      getLoadsMock.mockReturnValue([{ id: 1, x: 20, fx: 100, fy: -300, mz: 0 }])
+
+      getSupportsMock.mockReturnValue([
+        { id: 1, x: 0, direction: SupportDirection.Fx, name: 'R_Fx_1' },
+        { id: 1, x: 0, direction: SupportDirection.Fy, name: 'R_Fy_1' },
+        { id: 1, x: 0, direction: SupportDirection.Mz, name: 'R_Mz_1' },
+      ])
+      const solvedSupports1 = beamAnalyzer.solveReactionForces()
+      expect(solvedSupports1[0]).toBe(-100)
+      expect(solvedSupports1[1]).toBe(300)
+      expect(solvedSupports1[2]).toBe(6000)
+
+      getSupportsMock.mockReturnValue([
+        { id: 1, x: 0, direction: SupportDirection.Fx, name: 'R_Fx_1' },
+        { id: 1, x: 0, direction: SupportDirection.Mz, name: 'R_Mz_1' },
+        { id: 1, x: 0, direction: SupportDirection.Fy, name: 'R_Fy_1' },
+      ])
+      const solvedSupports2 = beamAnalyzer.solveReactionForces()
+      expect(solvedSupports2[0]).toBe(-100)
+      expect(solvedSupports2[1]).toBe(6000)
+      expect(solvedSupports2[2]).toBe(300)
+
+      getSupportsMock.mockReturnValue([
+        { id: 1, x: 0, direction: SupportDirection.Mz, name: 'R_Mz_1' },
+        { id: 1, x: 0, direction: SupportDirection.Fx, name: 'R_Fx_1' },
+        { id: 1, x: 0, direction: SupportDirection.Fy, name: 'R_Fy_1' },
+      ])
+      const solvedSupports3 = beamAnalyzer.solveReactionForces()
+      expect(solvedSupports3[0]).toBe(6000)
+      expect(solvedSupports3[1]).toBe(-100)
+      expect(solvedSupports3[2]).toBe(300)
+
+      getSupportsMock.mockReturnValue([
+        { id: 1, x: 0, direction: SupportDirection.Mz, name: 'R_Mz_1' },
+        { id: 1, x: 0, direction: SupportDirection.Fy, name: 'R_Fy_1' },
+        { id: 1, x: 0, direction: SupportDirection.Fx, name: 'R_Fx_1' },
+      ])
+      const solvedSupports4 = beamAnalyzer.solveReactionForces()
+      expect(solvedSupports4[0]).toBe(6000)
+      expect(solvedSupports4[1]).toBe(300)
+      expect(solvedSupports4[2]).toBe(-100)
+
+      getSupportsMock.mockReturnValue([
+        { id: 1, x: 0, direction: SupportDirection.Fy, name: 'R_Fy_1' },
+        { id: 1, x: 0, direction: SupportDirection.Fx, name: 'R_Fx_1' },
+        { id: 1, x: 0, direction: SupportDirection.Mz, name: 'R_Mz_1' },
+      ])
+      const solvedSupports5 = beamAnalyzer.solveReactionForces()
+      expect(solvedSupports5[0]).toBe(300)
+      expect(solvedSupports5[1]).toBe(-100)
+      expect(solvedSupports5[2]).toBe(6000)
+
+      getSupportsMock.mockReturnValue([
+        { id: 1, x: 0, direction: SupportDirection.Fy, name: 'R_Fy_1' },
+        { id: 1, x: 0, direction: SupportDirection.Mz, name: 'R_Mz_1' },
+        { id: 1, x: 0, direction: SupportDirection.Fx, name: 'R_Fx_1' },
+      ])
+      const solvedSupports6 = beamAnalyzer.solveReactionForces()
+      expect(solvedSupports6[0]).toBe(300)
+      expect(solvedSupports6[1]).toBe(6000)
+      expect(solvedSupports6[2]).toBe(-100)
+    })
   })
 })
