c5d11b30a21ad93664e205188a90f8e683dca5f9
Jack Agtual <jaagtual@gmail.com>
2023-10-27 10:46:49 -0700

    Adding validation to support form

--- src/components/BeamForm/SupportsForm.tsx
+++ src/components/BeamForm/SupportsForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { Beam, Support, SupportDirection } from '../../types/staticAnalysis'
 
 type SupportFormProps = {
@@ -8,14 +8,45 @@ type SupportFormProps = {
 
 function SupportsForm({ beam, setBeam }: SupportFormProps) {
   const [supportCnt, setSupportCnt] = useState(0)
+  const directionRef = useRef<HTMLInputElement | null>(null)
+  const locationRef = useRef<HTMLInputElement | null>(null)
+
+  function handleLocationChange() {
+    locationRef.current?.setCustomValidity('')
+  }
+
+  function handleDirectionChange() {
+    directionRef.current?.setCustomValidity('')
+  }
+
+  function supportExistsAtLocation(x: number) {
+    return beam.supports.some((support) => support.x === x)
+  }
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    const formData = new FormData(e.currentTarget)
+    console.log('submitting')
+    const form = e.currentTarget
+    const formData = new FormData(form)
     const x = Number(formData.get('location'))
     const rx = !!formData.get('rx')
     const ry = !!formData.get('ry')
     const mz = !!formData.get('mz')
+
+    if (supportExistsAtLocation(x)) {
+      locationRef.current?.setCustomValidity('Support already exists at location')
+    }
+    const noSupportSelected = !rx && !ry && !mz
+    if (noSupportSelected) {
+      directionRef.current?.setCustomValidity('Must select at least one support')
+    }
+
+    locationRef.current?.reportValidity()
+    directionRef.current?.reportValidity()
+
+    if (!locationRef.current?.checkValidity() || !directionRef.current?.checkValidity()) {
+      return
+    }
 
     const supports: Support[] = []
     const id = supportCnt
@@ -35,23 +66,38 @@ function SupportsForm({ beam, setBeam }: SupportFormProps) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <legend>Supports</legend>
       <label>
         <p>x location</p>
-        <input required type="number" name="location" min={0} max={beam.length ?? ''} />
+        <input
+          ref={locationRef}
+          required
+          type="number"
+          name="location"
+          min={0}
+          max={beam.length ?? ''}
+          onChange={handleLocationChange}
+        />
       </label>
-      <label>
-        <p>Force x</p>
-        <input type="checkbox" name="rx" />
-      </label>
-      <label>
-        <p>Force y</p>
-        <input type="checkbox" name="ry" />
-      </label>
-      <label>
-        <p>Moment z</p>
-        <input type="checkbox" name="mz" />
-      </label>
+      <fieldset>
+        <legend>Support direction</legend>
+        <label>
+          <p>Force x</p>
+          <input
+            ref={directionRef}
+            type="checkbox"
+            name="rx"
+            onChange={handleDirectionChange}
+          />
+        </label>
+        <label>
+          <p>Force y</p>
+          <input type="checkbox" name="ry" onChange={handleDirectionChange} />
+        </label>
+        <label>
+          <p>Moment z</p>
+          <input type="checkbox" name="mz" onChange={handleDirectionChange} />
+        </label>
+      </fieldset>
       <button type="submit">Add support</button>
     </form>
   )
