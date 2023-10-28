80853e789e3f1c7826920beb2901ff72eb8b1869
Jack Agtual <jaagtual@gmail.com>
2023-10-27 21:16:06 -0700

    Adding load table

--- src/components/TableDisplay/LoadsTable.tsx
+++ src/components/TableDisplay/LoadsTable.tsx
@@ -1,11 +1,36 @@
-import { BeamState, Load } from '../../types/staticAnalysis'
+import { Load } from '../../types/staticAnalysis'
 
 type LoadsTableProps = {
   loads: Load[]
 }
 
 function LoadsTable({ loads }: LoadsTableProps) {
-  return <div>LoadsTable</div>
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Location</th>
+          <th>Fx</th>
+          <th>Fy</th>
+          <th>Mz</th>
+        </tr>
+      </thead>
+      <tbody>
+        {loads.map((load) => {
+          return (
+            <tr key={load.id}>
+              <td>{load.id}</td>
+              <td>{load.x}</td>
+              <td>{load.fx}</td>
+              <td>{load.fy}</td>
+              <td>{load.mz}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }
 
 export default LoadsTable
