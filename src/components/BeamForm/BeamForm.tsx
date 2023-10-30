c6fc41464f13dd9bf25e4bb485b7eb2bb9302128
Jack Agtual <jaagtual@gmail.com>
2023-10-29 18:50:30 -0700

    mathjs dependency error

--- src/components/BeamForm/BeamForm.tsx
+++ src/components/BeamForm/BeamForm.tsx
@@ -2,10 +2,20 @@ import { BeamState } from '../../types/staticAnalysis'
 import LengthForm from './LengthForm'
 import SupportsForm from './SupportsForm'
 import LoadsForm from './LoadsForm'
+import BeamAnalyzer from '../../services/beam-analyzer/BeamAnalyzer'
 
 type BeamFormProps = BeamState
 
 function BeamForm({ beam, setBeam }: BeamFormProps) {
+  function solveBeam() {
+    const { supports, loads } = beam
+    const beamAnalyzer = new BeamAnalyzer({ supports, loads })
+    const supportValues = beamAnalyzer.solveReactionForces()
+    console.log(supportValues)
+  }
+
+  // TODO: Figure out import/dependency issue causing error in console
+
   return (
     <>
       <h1>Beam information</h1>
@@ -17,6 +27,9 @@ function BeamForm({ beam, setBeam }: BeamFormProps) {
           <LoadsForm beam={beam} setBeam={setBeam} />
         </>
       )}
+      <button type="button" onClick={solveBeam}>
+        Solve beam
+      </button>
     </>
   )
 }
